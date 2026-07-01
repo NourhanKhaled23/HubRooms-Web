f78bc4f29d66e81a37bac236e60339037a1c2a2e
Nourhan Khaled <1213084@dtu.edu.eg>
2026-07-01 05:45:25 +0300

    Update Web API to support JWT authentication for Desktop client

--- AdvancedChat.Web/Program.cs
+++ AdvancedChat.Web/Program.cs
@@ -13,7 +13,25 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddApiEndpoints();
+
+builder.Services.ConfigureAll<Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerOptions>(options =>
+{
+    options.Events = new Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Query["access_token"];
+            var path = context.HttpContext.Request.Path;
+            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/chatHub"))
+            {
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddSignalR();
 builder.Services.AddEndpointsApiExplorer();
@@ -48,6 +66,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+app.MapIdentityApi<IdentityUser>();
 app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
